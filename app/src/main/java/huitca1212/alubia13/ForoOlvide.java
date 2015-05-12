c6f1f16ee3fca885ce3e0fea48d771ae3e041374
Rodrigo Tugores Blanco <emiajtu@gmail.com>
2015-05-12 18:32:18 +0200

    Fixed: forgot password class

--- app/src/main/java/huitca1212/alubia13/ForoOlvide.java
+++ app/src/main/java/huitca1212/alubia13/ForoOlvide.java
@@ -41,32 +41,32 @@ public class ForoOlvide extends Activity {
         setContentView(R.layout.foro_olvide);
 
         pantalla_cargando = (LinearLayout) findViewById(R.id.progressbar_view_registro);
-        final EditText username_edit = (EditText) findViewById(R.id.user);
-        final Button boton = (Button) findViewById(R.id.button);
+        final EditText email = (EditText) findViewById(R.id._email);
+        final Button boton = (Button) findViewById(R.id._emailbutton);
 
-        // Al hacer click en el botón de enviar login
+        // Al hacer click en el botón de recuperar contrasenya
         boton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Comprobamos si el nombre de usuario está escrito correctamente
-                String username = username_edit.getText().toString();
-                if (username.length() < 3) {
-                    Toast.makeText(getApplicationContext(), "El nombre ha de tener al menos 3 caracteres", Toast.LENGTH_SHORT).show();
+                String s_email = email.getText().toString();
+                if (s_email.length() < 5) {
+                    Toast.makeText(getApplicationContext(), "El email no puede tener menos de 5 caracteres", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 try {
-                    mURL = "http://rjapps.x10host.com/recordar_contrasenya.php?email=" + URLEncoder.encode(username, "UTF-8");
+                    mURL = "http://rjapps.x10host.com/olvide_contrasenya.php?email=" + URLEncoder.encode(s_email, "UTF-8");
                     mURL = mURL.replace(" ", "%20");
                     Toast.makeText(getApplicationContext(), mURL, Toast.LENGTH_LONG).show();
                     // Chequear si está la conexión a Internet activa
                     if (!checkInternet()) {
-                        Toast.makeText(getApplicationContext(), "Necesitas conexión a Internet para registrarte.", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Necesitas conexión a Internet para solicitar tu contraseña.", Toast.LENGTH_LONG).show();
                         finish();
                     }
                     // Enviamos el registro
                     SendTask enviar = new SendTask();
                     enviar.execute(mURL);
                 } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Error con la recuperacion de información, verifique su conexión a Internet.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Error con la recuperacion de información, verifica tu conexión a Internet.", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -88,7 +88,7 @@ public class ForoOlvide extends Activity {
     }
 
     //====================================================================================================================
-    //Tarea para enviar registro
+    //Tarea para enviar consulta
     //====================================================================================================================
     private class SendTask extends AsyncTask<String, Void, String> {
         boolean error_envio = false;
@@ -124,26 +124,12 @@ public class ForoOlvide extends Activity {
         @Override
         protected void onPostExecute(String result) {
             if (error_envio) {
-                Toast.makeText(getApplicationContext(), "El registro no se ha completado correctamente. Revisa tu conexión a Internet.", Toast.LENGTH_LONG).show();
                 pantalla_cargando.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "La consulta no se ha completado correctamente. Revisa tu conexión a Internet.", Toast.LENGTH_LONG).show();
             } else {
                 pantalla_cargando.setVisibility(View.GONE);
-                Toast.makeText(getApplicationContext(), "El registro se ha completado con éxito.", Toast.LENGTH_SHORT).show();
-                final EditText username_edit = (EditText) findViewById(R.id.user);
-                String username = username_edit.getText().toString();
-                // Almacenamos el nombre de usuario en el teléfono
-                getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-                        .edit()
-                        .putString("username", username)
-                        .commit();
-                Intent intent = new Intent(ForoOlvide.this, Foro.class);
-                // Almacenamos que el usuario ya se ha registrado en el teléfono
-                getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-                        .edit()
-                        .putBoolean("notregister", false)
-                        .commit();
-                // Abrimos nueva actividad y cerramos ya esta
-                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "En unos segundos recibirás un email a tu correo con la contraseña.", Toast.LENGTH_LONG).show();
+                // Cerramos actividad
                 finish();
             }
         }
