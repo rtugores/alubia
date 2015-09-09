a299db15ba8741ec3c1f9dd3814e8b36ab142c39
Rodrigo Tugores Blanco <emiajtu@gmail.com>
2015-09-09 16:40:46 +0200

    Fixed: the Universal Image Loader is now working (but not as desired)

--- app/src/main/java/huitca1212/alubia13/MainActivity.java
+++ app/src/main/java/huitca1212/alubia13/MainActivity.java
@@ -3,24 +3,40 @@ package huitca1212.alubia13;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.utils.L;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import huitca1212.alubia13.foro.Foro;
 import huitca1212.alubia13.foro.ForoInicial;
 import huitca1212.alubia13.mas.Mas;
 import huitca1212.alubia13.novedades.Novedades;
-import huitca1212.alubia13.penyas.Penyas;
-import huitca1212.alubia13.penyas.SubirImagen;
+import huitca1212.alubia13.penyas.ComplexImageActivity;
 import huitca1212.alubia13.programa.Programa;
 
 
 public class MainActivity extends Activity {
 
+    private static final String TEST_FILE_NAME = "Universal Image Loader @#&=+-_.,!()~'%20.png";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        File testImageOnSdCard = new File("/mnt/sdcard", TEST_FILE_NAME);
+        if (!testImageOnSdCard.exists()) {
+            copyTestImageToSdCard(testImageOnSdCard);
+        }
 
         final Button boton1 = (Button) findViewById(R.id.button1); //PRIMER BOTON
         boton1.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +48,7 @@ public class MainActivity extends Activity {
         final Button boton2 = (Button) findViewById(R.id.button2); //SEGUNDO BOTON
         boton2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SubirImagen.class);
+                Intent intent = new Intent(MainActivity.this, ComplexImageActivity.class);
                 startActivity(intent);
             }
         });
@@ -67,4 +83,56 @@ public class MainActivity extends Activity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        ImageLoader.getInstance().stop();
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_clear_memory_cache:
+                ImageLoader.getInstance().clearMemoryCache();
+                return true;
+            case R.id.item_clear_disc_cache:
+                ImageLoader.getInstance().clearDiskCache();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void copyTestImageToSdCard(final File testImageOnSdCard) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InputStream is = getAssets().open(TEST_FILE_NAME);
+                    FileOutputStream fos = new FileOutputStream(testImageOnSdCard);
+                    byte[] buffer = new byte[8192];
+                    int read;
+                    try {
+                        while ((read = is.read(buffer)) != -1) {
+                            fos.write(buffer, 0, read);
+                        }
+                    } finally {
+                        fos.flush();
+                        fos.close();
+                        is.close();
+                    }
+                } catch (IOException e) {
+                    L.w("Can't copy test image onto SD card");
+                }
+            }
+        }).start();
+    }
+
 }
