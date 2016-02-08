c401321ef6944540b43178816fac24ef0a063cce
Rodrigo <rodrigo.tugores@mobgen.com>
2016-02-08 17:01:06 +0100

    Refactor of the register finalization

--- app/src/main/java/huitca1212/alubia13/business/ForumLoginRegisterBusiness.java
+++ app/src/main/java/huitca1212/alubia13/business/ForumLoginRegisterBusiness.java
@@ -14,7 +14,6 @@ public class ForumLoginRegisterBusiness {
 
 	public static void checkEmailForum(final String email, final boolean fromLogin, final ServerListenerInterface<String> listener) {
 		new DefaultAsyncTask(new AsyncTaskListenerInterface() {
-			Result result;
 
 			@Override
 			public void onStart() {
@@ -35,7 +34,7 @@ public class ForumLoginRegisterBusiness {
 					String jsonResult = response.body().string();
 					GsonBuilder gsonBuilder = new GsonBuilder();
 					Gson gson = gsonBuilder.create();
-					result = gson.fromJson(jsonResult, Result.class);
+					Result result = gson.fromJson(jsonResult, Result.class);
 					return result.getResult();
 				} catch (Exception e) {
 					return DefaultAsyncTask.ASYNC_TASK_ERROR;
@@ -64,7 +63,6 @@ public class ForumLoginRegisterBusiness {
 
 	public static void checkPasswordLoginForum(final String email, final String password, final ServerListenerInterface<String> listener) {
 		new DefaultAsyncTask(new AsyncTaskListenerInterface() {
-			Result result;
 
 			@Override
 			public void onStart() {
@@ -87,7 +85,7 @@ public class ForumLoginRegisterBusiness {
 					String jsonResult = response.body().string();
 					GsonBuilder gsonBuilder = new GsonBuilder();
 					Gson gson = gsonBuilder.create();
-					result = gson.fromJson(jsonResult, Result.class);
+					Result result = gson.fromJson(jsonResult, Result.class);
 					return result.getResult();
 				} catch (Exception e) {
 					return DefaultAsyncTask.ASYNC_TASK_ERROR;
@@ -107,7 +105,6 @@ public class ForumLoginRegisterBusiness {
 
 	public static void retriveForgottenPasswd(final String email, final ServerListenerInterface<String> listener) {
 		new DefaultAsyncTask(new AsyncTaskListenerInterface() {
-			Result result;
 
 			@Override
 			public void onStart() {
@@ -128,7 +125,7 @@ public class ForumLoginRegisterBusiness {
 					String jsonResult = response.body().string();
 					GsonBuilder gsonBuilder = new GsonBuilder();
 					Gson gson = gsonBuilder.create();
-					result = gson.fromJson(jsonResult, Result.class);
+					Result result = gson.fromJson(jsonResult, Result.class);
 					return result.getResult();
 				} catch (Exception e) {
 					return DefaultAsyncTask.ASYNC_TASK_ERROR;
@@ -148,7 +145,6 @@ public class ForumLoginRegisterBusiness {
 
 	public static void checkUserRegisterForum(final String user, final ServerListenerInterface<String> listener) {
 		new DefaultAsyncTask(new AsyncTaskListenerInterface() {
-			Result result;
 
 			@Override
 			public void onStart() {
@@ -169,7 +165,49 @@ public class ForumLoginRegisterBusiness {
 					String jsonResult = response.body().string();
 					GsonBuilder gsonBuilder = new GsonBuilder();
 					Gson gson = gsonBuilder.create();
-					result = gson.fromJson(jsonResult, Result.class);
+					Result result = gson.fromJson(jsonResult, Result.class);
+					return result.getResult();
+				} catch (Exception e) {
+					return DefaultAsyncTask.ASYNC_TASK_ERROR;
+				}
+			}
+
+			@Override
+			public void onFinish(String result) {
+				if (result.equals(DefaultAsyncTask.ASYNC_TASK_OK)) {
+					listener.onServerSuccess(result);
+				} else {
+					listener.onFailure(result);
+				}
+			}
+		}).execute();
+	}
+
+	public static void performRegistrationForum(final String user, final String password, final String email, final String code, final String mobileId,
+			final ServerListenerInterface<String> listener) {
+		new DefaultAsyncTask(new AsyncTaskListenerInterface() {
+
+			@Override
+			public void onStart() {
+			}
+
+			@Override
+			public String onBackground() {
+				OkHttpClient client = new OkHttpClient();
+
+				try {
+					String mUrl = "http://rjapps.x10host.com/comprobar_codigo.php?usuario=" + URLEncoder.encode(user, "UTF-8") +
+							"&contrasenya=" + URLEncoder.encode(password, "UTF-8") + "&email=" + URLEncoder.encode(email, "UTF-8") +
+							"&codigo_penya=" + URLEncoder.encode(code, "UTF-8") + "&mobile_id=" + URLEncoder.encode(mobileId, "UTF-8");
+					Request request = new Request.Builder()
+							.url(mUrl)
+							.build();
+
+					Response response = client.newCall(request).execute();
+					String jsonResult = response.body().string();
+					GsonBuilder gsonBuilder = new GsonBuilder();
+					Gson gson = gsonBuilder.create();
+					Result result = gson.fromJson(jsonResult, Result.class);
 					return result.getResult();
 				} catch (Exception e) {
 					return DefaultAsyncTask.ASYNC_TASK_ERROR;
--- app/src/main/java/huitca1212/alubia13/ui/forum/ForumRegisterCodeActivity.java
+++ app/src/main/java/huitca1212/alubia13/ui/forum/ForumRegisterCodeActivity.java
@@ -1,15 +1,7 @@
 package huitca1212.alubia13.ui.forum;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -24,25 +16,18 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URLEncoder;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import huitca1212.alubia13.R;
+import huitca1212.alubia13.business.DefaultAsyncTask;
+import huitca1212.alubia13.business.ForumLoginRegisterBusiness;
+import huitca1212.alubia13.business.ServerListenerInterface;
 import huitca1212.alubia13.ui.more.ContactActivity;
 
 public class ForumRegisterCodeActivity extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener {
 
-	private boolean error = false;
-	private String jsonResult, mURL;
-	private String user;
-	private String email;
-	private String password;
-	@Bind(R.id.progressbar_view_registro) LinearLayout progressBar;
+	private String user, email, password;
+	@Bind(R.id.progressbar_view_registro) LinearLayout progressbarView;
 	@Bind(R.id.register_button) Button registerButton;
 	@Bind(R.id.contact_button) Button contactButton;
 	@Bind(R.id.code_edit_text) EditText codeEditText;
@@ -56,19 +41,12 @@ public class ForumRegisterCodeActivity extends AppCompatActivity implements View
 		getWindow().setBackgroundDrawableResource(R.drawable.background_image);
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
-		codeEditText.setOnEditorActionListener(this);
+		Bundle extras = getIntent().getExtras();
+		user = extras.getString("usuario");
+		email = extras.getString("email");
+		password = extras.getString("contrasenya");
 
-		// Take username and other data from the previous activity
-		if (savedInstanceState == null) {
-			Bundle extras = getIntent().getExtras();
-			user = extras.getString("usuario");
-			email = extras.getString("email");
-			password = extras.getString("contrasenya");
-		} else {
-			user = (String)savedInstanceState.getSerializable("usuario");
-			email = (String)savedInstanceState.getSerializable("email");
-			password = (String)savedInstanceState.getSerializable("contrasenya");
-		}
+		codeEditText.setOnEditorActionListener(this);
 		registerButton.setOnClickListener(this);
 		contactButton.setOnClickListener(this);
 	}
@@ -77,7 +55,7 @@ public class ForumRegisterCodeActivity extends AppCompatActivity implements View
 	public void onClick(View v) {
 		int id = v.getId();
 		if (id == R.id.register_button) {
-			performRegisterCodeAction();
+			performRegistation();
 		} else if (id == R.id.contact_button) {
 			Intent intent = new Intent(ForumRegisterCodeActivity.this, ContactActivity.class);
 			startActivity(intent);
@@ -89,17 +67,16 @@ public class ForumRegisterCodeActivity extends AppCompatActivity implements View
 		int id = v.getId();
 		if (id == R.id.code_edit_text) {
 			if (actionId == EditorInfo.IME_ACTION_DONE) {
-				performRegisterCodeAction();
+				performRegistation();
 				return true;
 			}
 		}
 		return false;
 	}
 
-	private void performRegisterCodeAction() {
-		String mobile_id = "";
+	private void performRegistation() {
+		String mobileId;
 
-		// Hide keyboard
 		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
 		imm.hideSoftInputFromWindow(codeEditText.getWindowToken(), 0);
 
@@ -108,143 +85,66 @@ public class ForumRegisterCodeActivity extends AppCompatActivity implements View
 			Toast.makeText(getApplicationContext(), R.string.forum_error_bad_code, Toast.LENGTH_SHORT).show();
 			return;
 		}
-		// Convertimos para que nos entienda el servidor
+		// Put code zero if lenght = 0 in order to understand with the server
 		if (code.length() == 0) {
 			code = "0";
 		}
 		try {
-			mobile_id = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+			mobileId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
-			mURL = "http://rjapps.x10host.com/comprobar_codigo.php?usuario=" + URLEncoder.encode(user, "UTF-8") +
-					"&contrasenya=" + URLEncoder.encode(password, "UTF-8") + "&email=" + URLEncoder.encode(email, "UTF-8") +
-					"&codigo_penya=" + URLEncoder.encode(code, "UTF-8") + "&mobile_id=" + URLEncoder.encode(mobile_id, "UTF-8");
-			mURL = mURL.replace(" ", "%20");
-			// Enviamos el login
-			SendLoginAsyncTask enviar = new SendLoginAsyncTask();
-			enviar.execute(mURL);
-		} catch (Exception e) {
-			e.printStackTrace();
-			Toast.makeText(getApplicationContext(), R.string.common_internet_error, Toast.LENGTH_LONG).show();
-		}
-	}
-
-
-	private class SendLoginAsyncTask extends AsyncTask<String, Void, String> {
-		HttpClient httpclient = new DefaultHttpClient();
-
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			progressBar.setVisibility(View.VISIBLE);
+			mobileId = "0";
 		}
 
-		@Override
-		protected String doInBackground(String... params) {
-			HttpPost httppost = new HttpPost(mURL);
-			try {
-				HttpResponse response = httpclient.execute(httppost);
-				jsonResult = inputStreamToString(
-						response.getEntity().getContent()).toString();
-			} catch (IOException e) {
-				e.printStackTrace();
-				error = true;
-			}
-			return null;
-		}
-
-		private StringBuilder inputStreamToString(InputStream is) {
-			String rLine;
-			StringBuilder answer = new StringBuilder();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			try {
-				while ((rLine = rd.readLine()) != null) {
-					answer.append(rLine);
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				error = true;
-			}
-			return answer;
-		}
-
-		@Override
-		protected void onCancelled() {
-			httpclient.getConnectionManager().shutdown();
-			progressBar.setVisibility(View.GONE);
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			String resultado = "-1";
-			try {
-				JSONObject jsonResponse = new JSONObject(jsonResult);
-				resultado = jsonResponse.optString("resultado");
-			} catch (JSONException | NullPointerException e) {
-				e.printStackTrace();
-				error = true;
-			}
-			int resultado_int = Integer.parseInt(resultado);
-			switch (resultado_int) {
-				case -1:
-					Toast.makeText(getApplicationContext(), R.string.common_internet_error, Toast.LENGTH_LONG).show();
-					progressBar.setVisibility(View.GONE);
-					return;
-				case -2:
-					Toast.makeText(getApplicationContext(), R.string.forum_error_end_user_repeated, Toast.LENGTH_LONG).show();
-					progressBar.setVisibility(View.GONE);
-					return;
-				case -3:
-					Toast.makeText(getApplicationContext(), R.string.forum_error_end_email_repeated, Toast.LENGTH_LONG).show();
-					progressBar.setVisibility(View.GONE);
-					return;
-				case -4:
-					Toast.makeText(getApplicationContext(), R.string.forum_error_different_code, Toast.LENGTH_LONG).show();
-					progressBar.setVisibility(View.GONE);
-					return;
-			}
-			if (error) {
-				Toast.makeText(getApplicationContext(), R.string.common_internet_error, Toast.LENGTH_LONG).show();
-				progressBar.setVisibility(View.GONE);
-			} else {
-				// Almacenamos el nombre de usuario en el teléfono
-				getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-						.edit()
-						.putString("username", user)
-						.commit();
-				// Almacenamos que el usuario ya se ha registrado en el teléfono
-				getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-						.edit()
-						.putBoolean("notregister", false)
-						.commit();
-				// Abrimos nueva actividad y cerramos las anteriores y esta
-				Intent intent = new Intent(ForumRegisterCodeActivity.this, ForumActivity.class);
-				intent.putExtra(ForumActivity.INVITED_USER, "NOK");
-				startActivity(intent);
+		progressbarView.setVisibility(View.VISIBLE);
+		ForumLoginRegisterBusiness.performRegistrationForum(user, password, email, code, mobileId, new ServerListenerInterface<String>() {
+			@Override
+			public void onServerSuccess(String result) {
+				progressbarView.setVisibility(View.GONE);
+				getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putString("username", user).commit();
+				getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("notregister", false).commit();
 				try {
 					ForumMenuActivity.forumMenuActivity.finish();
 				} catch (NullPointerException e) {
-					e.printStackTrace();
+					//NOOP
 				}
 				try {
 					ForumRegisterUserActivity.forumRegisterUserActivity.finish();
 				} catch (NullPointerException e) {
-					e.printStackTrace();
+					//NOOP
 				}
 				try {
 					ForumRegisterEmailActivity.forumRegisterEmailActivity.finish();
 				} catch (NullPointerException e) {
-					e.printStackTrace();
+					//NOOP
 				}
 				try {
 					ForumRegisterPasswordActivity.forumRegisterPasswordActivity.finish();
 				} catch (NullPointerException e) {
-					e.printStackTrace();
+					//NOOP
 				}
+				Intent intent = new Intent(ForumRegisterCodeActivity.this, ForumActivity.class);
+				intent.putExtra(ForumActivity.INVITED_USER, "NOK");
+				startActivity(intent);
 				finish();
 			}
-		}
+
+			@Override
+			public void onFailure(String result) {
+				progressbarView.setVisibility(View.GONE);
+				switch (result) {
+					case DefaultAsyncTask.ASYNC_TASK_ERROR:
+						Toast.makeText(getApplicationContext(), R.string.common_internet_error, Toast.LENGTH_LONG).show();
+						break;
+					case "-2":
+						Toast.makeText(getApplicationContext(), R.string.forum_error_end_user_repeated, Toast.LENGTH_LONG).show();
+						break;
+					case "-3":
+						Toast.makeText(getApplicationContext(), R.string.forum_error_end_email_repeated, Toast.LENGTH_LONG).show();
+						break;
+					case "-4":
+						Toast.makeText(getApplicationContext(), R.string.forum_error_different_code, Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 	}
 }
