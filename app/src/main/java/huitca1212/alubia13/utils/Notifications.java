5a8265ba6fe470464f4e7b574832e03063851293
Rodrigo Tugores Blanco <emiajtu@gmail.com>
2016-03-22 10:21:44 +0100

    Fix the snackbar color

--- app/src/main/java/huitca1212/alubia13/utils/Notifications.java
+++ app/src/main/java/huitca1212/alubia13/utils/Notifications.java
@@ -1,14 +1,21 @@
 package huitca1212.alubia13.utils;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Notifications {
 
 	public static void showSnackBar(CoordinatorLayout coordinatorLayout, String text) {
-		Snackbar.make(coordinatorLayout, text, Snackbar.LENGTH_LONG).show();
+		Snackbar mySnackbar = Snackbar.make(coordinatorLayout, text, Snackbar.LENGTH_LONG);
+		View view = mySnackbar.getView();
+		TextView textView = (TextView)view.findViewById(android.support.design.R.id.snackbar_text);
+		textView.setTextColor(Color.WHITE);
+		mySnackbar.show();
 	}
 
 	public static void showToast(Context ctx, String text) {
