d7a1d6a43e08315bdce37fb0fb19a400472c952b
Rodrigo Tugores Blanco <emiajtu@gmail.com>
2016-05-29 17:25:40 +0200

    Only set ads and analytics when release buildtype

--- app/src/main/java/huitca1212/alubia13/utils/AdsAndAnalytics.java
+++ app/src/main/java/huitca1212/alubia13/utils/AdsAndAnalytics.java
@@ -10,31 +10,37 @@ import android.content.Context;
 import android.os.Handler;
 import android.view.View;
 
+import huitca1212.alubia13.BuildConfig;
+
 public class AdsAndAnalytics {
 
 	public static void setAnalytics(Context ctx) {
-		GoogleAnalytics analytics = GoogleAnalytics.getInstance(ctx);
-		analytics.setLocalDispatchPeriod(1800);
-		Tracker tracker = analytics.newTracker("UA-42496077-1");
-		tracker.enableExceptionReporting(true);
-		tracker.enableAdvertisingIdCollection(true);
-		tracker.enableAutoActivityTracking(true);
+		if (BuildConfig.BUILD_TYPE.equals("release")) {
+			GoogleAnalytics analytics = GoogleAnalytics.getInstance(ctx);
+			analytics.setLocalDispatchPeriod(1800);
+			Tracker tracker = analytics.newTracker("UA-42496077-1");
+			tracker.enableExceptionReporting(true);
+			tracker.enableAdvertisingIdCollection(true);
+			tracker.enableAutoActivityTracking(true);
+		}
 	}
 
 	public static void loadAds(final AdView adView) {
-		final Handler handler = new Handler();
-		handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				AdRequest adRequest = new AdRequest.Builder().build();
-				adView.loadAd(adRequest);
-				adView.setAdListener(new AdListener() {
-					@Override
-					public void onAdLoaded() {
-						adView.setVisibility(View.VISIBLE);
-					}
-				});
-			}
-		}, 200);
+		if (BuildConfig.BUILD_TYPE.equals("release")) {
+			final Handler handler = new Handler();
+			handler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					AdRequest adRequest = new AdRequest.Builder().build();
+					adView.loadAd(adRequest);
+					adView.setAdListener(new AdListener() {
+						@Override
+						public void onAdLoaded() {
+							adView.setVisibility(View.VISIBLE);
+						}
+					});
+				}
+			}, 200);
+		}
 	}
 }
